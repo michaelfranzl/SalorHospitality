ff26cdc2df37f8186854a4383a1f2fe206054cd0
Michael Franzl <m.franzl@aon.at>
2009-02-13 13:51:55 +0100

    Changed Model definitions

--- config/routes.rb
+++ config/routes.rb
@@ -31,11 +31,12 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+   map.root :controller => 'table'
 
   # See how all your routes lay out with "rake routes"
   map.resources :articles
   map.resources :categories
+  map.resources :tables, :has_many :orders
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
--- db/migrate/20090209124114_create_tables.rb
+++ db/migrate/20090209124114_create_tables.rb
@@ -2,6 +2,7 @@ class CreateTables < ActiveRecord::Migration
   def self.up
     create_table :tables do |t|
       t.string :name
+      t.string :description
 
       t.timestamps
     end
