e7b8a56ba9a27b09ee17c462e287411922a28cc2
Michael Franzl <me@michaelfranzl.com>
2010-03-06 18:51:19 +0100

    item printing happens now by order

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,45 +1,50 @@
 class ItemsController < ApplicationController
 
   def print
-    @unprinted_items = Item.find_all_by_printed(false)
     respond_to do |wants|
       wants.html
       wants.bon {
-        @unprinted_items.each do |ui|
-          ui.update_attribute(:printed, true)
-        end
-        from = @unprinted_items.size - 21
-        from = 0 if from < 0
-        to = @unprinted_items.size - 1
-        @print_items = @unprinted_items[from..to]
-        render :text => generate_escpos_items(@print_items)
+        render :text => generate_escpos_items
       }
     end
   end
 
 
   private
-    def generate_escpos_items(unprinted_items)
+    def generate_escpos_items
 
       output = ''
-      unprinted_items.each do |ui|
-        next if ui.article.category.tax.percent != 10  # This is ugly but works for now
+      Order.find_all_by_finished(false).each do |order|
 
         output +=
         "\e@"     +  # Initialize Printer
 
         "\e!\x00" +  # Font A
         "\e!\x08" +  # Font A, emphasized
-        "%-25.25s %15s\n" % [ui.order.user.title, ui.order.table.name] +
+        "%-25.25s %15s\n" % [order.user.title, order.table.name] +
+        "\n\n\n"
 
-        "\e!\x38" +  # doube tall, double wide, bold
-        "%u %-17.17s\n" % [ui.count, ui.article.name] +
+        unprinted_items = Item.find(:all, :conditions => { :order_id => order.id, :printed => false } )
+        return '' if unprinted_items.size.zero?
+        unprinted_items.each do |ui|
+          ui.update_attribute(:printed, true)
+          next if ui.article.category.tax.percent != 10  # This is ugly but works for now
 
-        "\e!\x08" +  # Font A, emphasized
-        "%-42.42s\n" % [ui.article.description] +
+          output +=
 
+          "\e!\x38" +  # doube tall, double wide, bold
+          "%u %-17.17s\n" % [ui.count, ui.article.name] +
+
+          "\e!\x08" +  # Font A, emphasized
+          "%-42.42s\n" % [ui.article.description] +
+
+          "\n\n\n"
+        end
+
+        output +=
         "\n\n\n\n\n\n" +
         "\x1DV\x00" # paper cut
+
       end
 
       output = Iconv.conv('ISO-8859-15','UTF-8',output)
