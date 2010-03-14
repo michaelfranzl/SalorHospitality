41ec1ab1f8c3422842ea94dfd487f73d98ce48c5
Michael Franzl <me@michaelfranzl.com>
2010-03-14 21:39:14 +0100

    added calculation ability

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -29,7 +29,7 @@ module OrdersHelper
     categories.collect{ |cat|
       cat.articles_in_menucard.collect{ |art|
         art.quantities.collect{ |qu|
-          "\nitemdetails_q[#{ qu.id }] = new Array( '#{ qu.article.id }', '#{ qu.article.name }', '#{ qu.name }', '#{ qu.article.price }', '#{ qu.article.description }', '#{ compose_item_label(qu) }');"
+          "\nitemdetails_q[#{ qu.id }] = new Array( '#{ qu.article.id }', '#{ qu.article.name }', '#{ qu.name }', '#{ qu.price }', '#{ qu.article.description }', '#{ compose_item_label(qu) }');"
         }.to_s
       }.to_s
     }.to_s
@@ -48,10 +48,11 @@ module OrdersHelper
     @sort = 'SORT'
     @articleid = 'ARTICLEID'
     @quantityid = 'QUANTITYID'
+    @price = 'PRICE'
     @label = 'LABEL'
     @count = 1
 
-    new_item_html = render 'items/item', :locals => { :sort => @sort, :articleid => @articleid, :quantityid => @quantityid, :label => @label, :designator => @designator, :count => @count, :increment_item_js => @increment_item_js, :decrement_item_js => @decrement_item_js }
+    new_item_html = render 'items/item', :locals => { :sort => @sort, :articleid => @articleid, :quantityid => @quantityid, :label => @label, :designator => @designator, :count => @count, :price => @price }
     new_item_html_var = "\n\nvar new_item_html = \"#{ escape_javascript new_item_html }\""
 
     return articleslist + quantitylist + itemdetails_a + itemdetails_q + new_item_html_var
@@ -81,9 +82,11 @@ module OrdersHelper
                       new_item_html_modified = new_item_html.replace(/DESIGNATOR/g, desig);
                       new_item_html_modified = new_item_html_modified.replace(/SORT/g, sort );
                       new_item_html_modified = new_item_html_modified.replace(/LABEL/g,  itemdetails_q[qu_id][5] );
+                      new_item_html_modified = new_item_html_modified.replace(/PRICE/g,  itemdetails_q[qu_id][3] );
                       new_item_html_modified = new_item_html_modified.replace(/ARTICLEID/g, itemdetails_q[qu_id][0] );
                       new_item_html_modified = new_item_html_modified.replace(/QUANTITYID/g, qu_id );
                       $('itemstable').insert({ top: new_item_html_modified });
+                      $('order_sum').value = parseFloat($('order_sum').value) + parseFloat(itemdetails_q[qu_id][3]);
                     }"
     add_new_item_a = "function add_new_item_a(art_id) {
                       var timestamp = new Date().getTime();
@@ -92,14 +95,17 @@ module OrdersHelper
                       new_item_html_modified = new_item_html.replace(/DESIGNATOR/g, desig);
                       new_item_html_modified = new_item_html_modified.replace(/SORT/g, sort);
                       new_item_html_modified = new_item_html_modified.replace(/LABEL/g,  itemdetails_a[art_id][5] );
+                      new_item_html_modified = new_item_html_modified.replace(/PRICE/g,  itemdetails_a[art_id][3] );
                       new_item_html_modified = new_item_html_modified.replace(/ARTICLEID/g, itemdetails_a[art_id][0] );
                       new_item_html_modified = new_item_html_modified.replace(/QUANTITYID/g, '' );
                       document.getElementById('quantitiestable').innerHTML = '&nbsp;';
                       $('itemstable').insert({ top: new_item_html_modified });
+                      $('order_sum').value = parseFloat($('order_sum').value) + parseFloat(itemdetails_a[art_id][3]);
                     }"
                     
     increment_item_func = "function increment_item(desig) {
                              $('count_' + desig).innerHTML = $('order_items_attributes_' + desig + '_count').value++ + 1;
+                             $('order_sum').value = parseFloat($('order_sum').value) + parseFloat($(desig + '_price').value);
                            }"
                            
     decrement_item_func = "function decrement_item(desig) {
@@ -107,18 +113,24 @@ module OrdersHelper
                              i = parseInt($('order_items_attributes_' + desig + '_count').value);
                              if (i < 2) { Effect.DropOut('item_' + desig); };
                              $('count_' + desig).innerHTML = $('order_items_attributes_' + desig + '_count').value-- - 1;
+                             $('order_sum').value = parseFloat($('order_sum').value) - parseFloat($(desig + '_price').value);
                            }"
+    remove_item_func = "function remove_item(desig) {
+                             Effect.DropOut('item_' + desig );
+                             $('order_items_attributes_' + desig + '__delete').value = 1;
+                             $('order_sum').value = parseFloat($('order_sum').value) - ( parseFloat($('order_items_attributes_' + desig + '_count').value) * parseFloat($(desig + '_price').value));
 
+                        }"
     
-    return display_articles + display_quantities + add_new_item_q + add_new_item_a + increment_item_func + decrement_item_func + flash_button
+    return display_articles + display_quantities + add_new_item_q + add_new_item_a + increment_item_func + decrement_item_func + flash_button + remove_item_func
   end
 
   def compose_item_label(input)
     if input.class == Quantity
-      label = "#{ input.article.name }<br><small>#{ input.name }</small>"
+      label = "#{ input.article.name }<br><small>#{ input.price } EUR, #{ input.name }</small>"
       #label += '<small>, ' + input.article.description if !input.article.description.empty?
     else
-      label = "#{ input.name }"
+      label = "#{ input.name }<br><small>#{ input.price } EUR</small>"
     end
     return label
   end
