a59c105e46feec0e6d94e6696aace90f4574f0fc
Michael Franzl <office@michaelfranzl.com>
2010-04-16 17:00:16 +0200

    improved order/new view a lot

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,15 @@
 module ApplicationHelper
 
   def toggle_admin_interface
-  
     toggle   =  "function toggle_admin_interface() {
-                         Effect.toggle('admin', 'slide');
-                  }"
+                   Effect.toggle('admin', 'slide');
+                 }"
     return toggle
   end
 
+  def onload_functions
+    function = 'function onload_function() {}'
+    function = 'function onload_function() { document.getElementById("order_sum").value = "33"; }'
+    return function
+  end
 end
