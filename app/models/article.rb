7b49c80d96e3451789a970782903de6f030add32
Michael Franzl <office@michaelfranzl.com>
2010-04-16 15:07:05 +0200

    correct price validation of articles and quantities, better articles/new form

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,9 +10,17 @@ class Article < ActiveRecord::Base
 
   validates_presence_of :name, :type, :category_id
   
-  validates_each :price do |record, attr, value|
-    record.errors.add attr, 'muss entweder beim Artikel selbst oder bei der Artikelmenge angegeben werden. ' if record.quantities.empty? and !value
-    validates_numericality_of :price
+  validates_each :price do |record, attr_name, value|
+    record.errors.add(attr_name, 'muss entweder beim Artikel selbst oder bei der Artikelmenge angegeben werden.') if record.quantities.empty? and !value
+    
+    if record.quantities.empty?
+      raw_value = record.send("#{attr_name}_before_type_cast") || value
+      begin
+        raw_value = Kernel.Float(raw_value)
+      rescue ArgumentError, TypeError
+        record.errors.add(attr_name, 'ist keine Zahl', :value => raw_value)
+      end
+    end
   end
   
   #code inspiration from http://ryandaigle.com/articles/2009/2/1/what-s-new-in-edge-rails-nested-attributes
@@ -21,7 +29,7 @@ class Article < ActiveRecord::Base
     :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 
   accepts_nested_attributes_for :quantities, :allow_destroy => true,
-    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+    :reject_if => proc { |attrs| attrs['name'] == '' && attrs['price'] == '' }
 
   def name_description
     descr = (description.nil? or description.empty?) ? '' : ("  |  " + description)
