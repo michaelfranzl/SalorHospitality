92cb5705fd59b79941709ff6d34e72576413efc6
Michael Franzl <michaelfranzl@gmx.at>
2010-08-15 14:51:27 +0200

    itemlist now fully working on webkit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -12,14 +12,14 @@ function add_new_item_q(qu_id) {
   var sort = timestamp.toString().substr(-9,9);
   var desig = 'new_' + sort;
 
-  new_item_tablerow_modified = new_item_tablerow.replace(/DESIGNATOR/,desig).replace(/SORT/,sort).replace(/LABEL/,itemdetails_q[qu_id][5]).replace(/PRICE/,itemdetails_q[qu_id][3]).replace(/ARTICLEID/,itemdetails_q[qu_id][0]).replace(/QUANTITYID/,qu_id);
+  new_item_tablerow_modified = new_item_tablerow.replace(/DESIGNATOR/g,desig).replace(/SORT/g,sort).replace(/LABEL/g,itemdetails_q[qu_id][5]).replace(/PRICE/g,itemdetails_q[qu_id][3]).replace(/ARTICLEID/g,itemdetails_q[qu_id][0]).replace(/QUANTITYID/g,qu_id);
 
-  new_item_inputfields_modified = new_item_inputfields.replace(/DESIGNATOR/,desig).replace(/SORT/,sort).replace(/LABEL/,itemdetails_q[qu_id][5]).replace(/PRICE/,itemdetails_q[qu_id][3]).replace(/ARTICLEID/,itemdetails_q[qu_id][0]).replace(/QUANTITYID/,qu_id);
+  new_item_inputfields_modified = new_item_inputfields.replace(/DESIGNATOR/g,desig).replace(/SORT/g,sort).replace(/LABEL/g,itemdetails_q[qu_id][5]).replace(/PRICE/g,itemdetails_q[qu_id][3]).replace(/ARTICLEID/g,itemdetails_q[qu_id][0]).replace(/QUANTITYID/g,qu_id);
 
   $('itemstable').insert({ top: new_item_tablerow_modified });
   $('inputfields').insert({ top: new_item_inputfields_modified });
-  //var sum = calculate_sum();
-  //$('order_sum').value = sum.toFixed(2).replace('.', ',');
+  var sum = calculate_sum();
+  $('order_sum').value = sum.toFixed(2).replace('.', ',');
 }
 
 function add_new_item_a(art_id) {
@@ -27,16 +27,16 @@ function add_new_item_a(art_id) {
   var sort = timestamp.toString().substr(-9,9);
   var desig = 'new_' + sort;
 
-  new_item_tablerow_modified = new_item_tablerow.replace(/DESIGNATOR/,desig).replace(/SORT/,sort).replace(/LABEL/,itemdetails_a[art_id][5]).replace(/PRICE/,itemdetails_a[art_id][3]).replace(/ARTICLEID/,itemdetails_a[art_id][0]).replace(/QUANTITYID/,'');
+  new_item_tablerow_modified = new_item_tablerow.replace(/DESIGNATOR/g,desig).replace(/SORT/g,sort).replace(/LABEL/g,itemdetails_a[art_id][5]).replace(/PRICEg/,itemdetails_a[art_id][3]).replace(/ARTICLEID/g,itemdetails_a[art_id][0]).replace(/QUANTITYID/g,'');
 
-  new_item_inputfields_modified = new_item_inputfields.replace(/DESIGNATOR/,desig).replace(/SORT/,sort).replace(/LABEL/,itemdetails_a[art_id][5]).replace(/PRICE/,itemdetails_a[art_id][3]).replace(/ARTICLEID/,itemdetails_a[art_id][0]).replace(/QUANTITYID/,'');
+  new_item_inputfields_modified = new_item_inputfields.replace(/DESIGNATOR/g,desig).replace(/SORT/g,sort).replace(/LABEL/g,itemdetails_a[art_id][5]).replace(/PRICE/g,itemdetails_a[art_id][3]).replace(/ARTICLEID/g,itemdetails_a[art_id][0]).replace(/QUANTITYID/g,'');
 
   $('itemstable').insert({ top: new_item_tablerow_modified });
   $('inputfields').insert({ top: new_item_inputfields_modified });
   document.getElementById('quantitiestable').innerHTML = '&nbsp;';
 
-  //var sum = calculate_sum();
-  //$('order_sum').value = sum.toFixed(2).replace('.', ',');
+  var sum = calculate_sum();
+  $('order_sum').value = sum.toFixed(2).replace('.', ',');
 }
 
 function increment_item(desig) {
@@ -72,7 +72,7 @@ function deselect_all_categories() {
   var container = document.getElementById("categories");
   var cats = container.children;
   for(c in cats) {
-    //cats[c].style.borderColor = "#555555 #222222 #222222 #555555";
+    cats[c].style.borderColor = "#555555 #222222 #222222 #555555";
   }
 }
 
@@ -81,21 +81,22 @@ function deselect_all_articles() {
   var container = document.getElementById("articlestable");
   var arts = container.rows;
   for(count in arts) {
-    //arts[count].firstChild.style.borderColor = "#555555 #222222 #222222 #555555";
+    arts[count].firstChild.style.borderColor = "#555555 #222222 #222222 #555555";
   }
 }
 
 function remove_item(desig) {
   Effect.DropOut('item_' + desig );
-  $('order_items_attributes_' + desig + '__delete').value = 1;
+  //$('item_' + desig ).remove();
+  $('order_items_attributes_' + desig + '__destroy').value = 1;
   $('order_items_attributes_' + desig + '_count').value = 0;
   var sum = calculate_sum();
   $('order_sum').value = sum.toFixed(2).replace('.', ',');
 }
 
 function calculate_sum() {
-  var prices = $$("#itemstable .price");
-  var counts = $$("#itemstable .count");
+  var prices = $$("#inputfields .price");
+  var counts = $$("#inputfields .count");
   var sum = 0;
   for(i=0; i<prices.length; i++) {
     sum += parseFloat(prices[i].value) * parseFloat(counts[i].value);
