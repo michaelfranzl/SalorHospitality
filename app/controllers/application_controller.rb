5a925c270e3520f136867f5cbdd4cb59f4b42d6c
root <root@billgastro.(none)>
2010-10-04 21:49:36 +0200

    session bugfix, added title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def fetch_logged_in_user
-      @current_user = User.find session[:user_id]
+      @current_user = User.find session[:user_id] if session[:user_id]
       redirect_to new_session_path and return unless @current_user
     end
 
