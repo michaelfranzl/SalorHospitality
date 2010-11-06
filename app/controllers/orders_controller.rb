fe05e76535f8971813812a9318149e21f691e37b
Michael Franzl <michaelfranzl@gmx.at>
2010-11-06 20:52:21 +0100

    new function: move items to another table

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -38,7 +38,6 @@ class OrdersController < ApplicationController
     @cost_centers = CostCenter.find(:all, :conditions => { :active => 1 })
     @order.table_id = params[:table_id]
     @order.sum = calculate_order_sum @order
-    @order.table_id = params[:move_order_to_table] if params[:move_order_to_table]
     @order.save ? process_order(@order) : render(:new)
   end
 
@@ -47,7 +46,6 @@ class OrdersController < ApplicationController
     @categories = Category.all
     @cost_centers = CostCenter.find(:all, :conditions => { :active => 1 })
     if @order.update_attributes(params[:order])
-      @order.update_attribute(:table_id, params[:move_order_to_table]) if params[:move_order_to_table]
       @order = Order.find(params[:id]) # re-read
       process_order @order
     else
@@ -163,6 +161,9 @@ class OrdersController < ApplicationController
           redirect_to orders_path
         when 'go_to_invoice'
           redirect_to table_path(order.table)
+        when 'move_order_to_table'
+          order = move_order_to_table(order, params[:target_table])
+          redirect_to table_path(params[:target_table])
       end
 
       File.open('bar.escpos', 'w') { |f| f.write(generate_escpos_items(:drink)) }
@@ -175,6 +176,15 @@ class OrdersController < ApplicationController
       `cat kitchen-takeaway.escpos > /dev/ttyPS0` #0 = Kitchen
 
       order.update_attribute( :sum, calculate_order_sum(order) )
+    end
+
+    def move_order_to_table(order,table_id)
+      @target_order = Order.find(:all, :conditions => { :table_id => table_id, :finished => false }).first
+      order.items.each do |i|
+        i.update_attribute :order, @target_order
+      end
+      @order.destroy
+      return @target_order
     end
 
     def reduce_stocks(order)
