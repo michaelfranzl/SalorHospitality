eb05c6531f0f85fb00278efa6c42626a743d8a25
Michael Franzl <michaelfranzl@gmx.at>
2010-11-19 17:27:03 +0100

    redirect if there is an error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,12 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   before_filter :fetch_logged_in_user
+  helper_method :logged_in?, :ipod?
 
-  protected
+  rescue_from ActionController::RoutingError, :with => :route_not_found
+  rescue_from ActionController::MethodNotAllowed, :with => :invalid_method
+
+  private
 
     def fetch_logged_in_user
       @current_user = User.find session[:user_id] if session[:user_id]
@@ -22,7 +26,15 @@ class ApplicationController < ActionController::Base
       (request.user_agent[13..16] == 'iPod') or (request.user_agent[13..16] == 'iPho') or params[:ipod]
     end
 
-    helper_method :logged_in?, :ipod?
+    def route_not_found
+      redirect_to orders_path
+    end
+
+    def invalid_method
+      redirect_to orders_path
+    end
+
+    
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
