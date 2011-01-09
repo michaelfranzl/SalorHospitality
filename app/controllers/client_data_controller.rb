5ed87cc9d5655eb94596cd89a5b4c5125d59990d
Michael Franzl <michaelfranzl@gmx.at>
2011-01-09 10:21:20 +0100

    client_data working

--- app/controllers/client_data_controller.rb
+++ app/controllers/client_data_controller.rb
@@ -1,15 +1,13 @@
 class ClientDataController < ApplicationController
 
   def index
+    File.open('config/client_data.yaml', 'w') { |out|  YAML.dump(params[:data], out) } if params[:data]
+
     if File.exist?('config/client_data.yaml')
       @client_data = YAML.load_file( 'config/client_data.yaml' )
     else
       @client_data = {}
     end
-  end
-
-  def update
-    File.open('config/client_data.yaml', 'w') { |out|  YAML.dump(params[:data], out) }
   end
 
 end
