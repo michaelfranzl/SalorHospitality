aae410ea399df08ea992ef156f7ebcab79d46684
Michael Franzl <michaelfranzl@gmx.at>
2011-04-16 15:51:40 +0200

    added client_data.ym with BillGastro advertising

--- config/packages/makepackage.sh
+++ config/packages/makepackage.sh
@@ -6,21 +6,21 @@ rm -rf $1/../publish/debian/db
 rm -rf $1/../publish/debian/dists
 rm -rf $1/../publish/debian/pool
 
+mkdir -p $1/../publish/debian
+
 echo "Copy compiled Ruby, Gems and configuration files"
 cp -rf ~/Public/pack/* $1
 
 echo "Copy BillGastro Source from git repository"
 mkdir -p $1/var/www
-cd $1/var/www
-git clone -b billgastro3 ../..
-rm -rf billgastro/.git
-cd -
+git clone -b billgastro3 . $1/var/www
+rm -rf $1/var/www/billgastro/.git
 
 dpkg -b $1
 reprepro -b $1/../publish/debian includedeb maverick $1.deb
 
 rm -rf /var/www/80/debian
-cp -r publish/debian /var/www/80
+cp -r $1/../publish/debian /var/www/80
 
 ncftpput -R -u billgastroweb-www -p QXP3mSUq -m billgastro.com public $1/../publish/debian
 
