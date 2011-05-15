8f52c4b25fc9fe54464171ad0ff6a2582e1bd48a
Michael Franzl <michaelfranzl@gmx.at>
2011-05-15 17:48:45 +0200

    ported rest to jquery

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -22,17 +22,6 @@ module ArticlesHelper
       page << %{
         var new_ingredient_id = "new_" + new Date().getTime();
         $('ingredients').insert({ bottom: '<tr id="' + new_ingredient_id + '">#{ escape_javascript ingredient }</tr>'.replace(/\\d/g, new_ingredient_id) });
-        function HighlightEffect(element){
-          new Effect.Highlight(element,
-            {
-              startcolor: "#3f5763",
-              endcolor: "#243239",
-              restorecolor: "#243239",
-              duration: 1
-            }
-          )
-        }
-        HighlightEffect($(new_ingredient_id));
       }
     end
   end
@@ -43,17 +32,6 @@ module ArticlesHelper
       page << %{
         var new_quantity_id = "new_" + new Date().getTime();
         $('quantities_new').insert({ bottom: '<tr id="' + new_quantity_id + '">#{ escape_javascript quantity }</tr>'.replace(/\\d/g, new_quantity_id) });
-        function HighlightEffect(element){
-          new Effect.Highlight(element,
-            {
-              startcolor: "#3f5763",
-              endcolor: "#243239",
-              restorecolor: "#243239",
-              duration: 1
-            }
-          )
-        }
-        HighlightEffect($(new_quantity_id));
       }
     end
   end
