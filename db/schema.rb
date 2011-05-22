70f1ca53325d75b99f4a7b9e92f30b1e4be6654c
Michael Franzl <michaelfranzl@gmx.at>
2011-05-22 16:38:27 +0200

    converted translations to utf8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110520135529) do
+ActiveRecord::Schema.define(:version => 20110513200843) do
 
   create_table "articles", :force => true do |t|
     t.string   "name"
