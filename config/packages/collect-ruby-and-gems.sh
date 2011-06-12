2dd57b8a674b9faee36a5ed84289069b6eba1b1c
Michael Franzl <michaelfranzl@gmx.at>
2011-06-12 14:39:29 +0200

    bumped up version to 3.2.3

--- config/packages/collect-ruby-and-gems.sh
+++ config/packages/collect-ruby-and-gems.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 
-rm -rf /media/sf_Public/pack
-mkdir -p /media/sf_Public/pack/usr/local/lib/ruby/gems/1.9.1
+rm -rf /media/sf_Public/opt
+mkdir -p /media/sf_Public/opt/billgastro
 
-cp -vpr /usr/local/bin /media/sf_Public/pack/usr/local
-cp -vpr /usr/local/include /media/sf_Public/pack/usr/local
+cd /media/sf_Public/opt/billgastro
 
-cp -vpr /usr/local/lib/ruby/1.9.1 /media/sf_Public/pack/usr/local/lib/ruby
-cp -vpr /usr/local/lib/ruby/gems/1.9.1/cache /media/sf_Public/pack/usr/local/lib/ruby/gems/1.9.1
-cp -vpr /usr/local/lib/ruby/gems/1.9.1/gems /media/sf_Public/pack/usr/local/lib/ruby/gems/1.9.1
-cp -vpr /usr/local/lib/ruby/gems/1.9.1/specifications /media/sf_Public/pack/usr/local/lib/ruby/gems/1.9.1
+cp -r /opt/billgastro/bin .
+cp -r /opt/billgastro/include .
+cp -r /opt/billgastro/lib .
 
-cp -vpr /usr/local/lib/libruby-static.a /media/sf_Public/pack/usr/local/lib
-
-mkdir -p /media/sf_Public/pack/etc/apache2/mods-enabled
-mkdir -p /media/sf_Public/pack/etc/apache2/sites-enabled
-mkdir -p /media/sf_Public/pack/etc/profile.d
-
-cp -vpr /etc/apache2/mods-enabled/rails* /media/sf_Public/pack/etc/apache2/mods-enabled
-cp -vpr /etc/apache2/sites-enabled/billgastro /media/sf_Public/pack/etc/apache2/sites-enabled
-cp -vpr /etc/profile.d/billgastro.sh /media/sf_Public/pack/etc/profile.d
+# delete not needed files to reduce download size
+rm -rf lib/ruby/gems/1.9.1/cache
+rm -rf lib/ruby/gems/1.9.1/doc
+rm -rf lib/ruby/gems/1.9.1/gems/passenger-3.0.7/doc
