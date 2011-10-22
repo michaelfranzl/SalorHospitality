43f30c5d0ba5e6ee06a6d18de141690b55a9ccde
Michael Franzl <michaelfranzl@gmx.at>
2011-10-22 19:07:02 +0200

    fixed table bug

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -17,7 +17,7 @@
 class TablesController < ApplicationController
 
   def index
-    @tables = Table.existing
+    @tables = @current_user.tables.existing
     @last_finished_order = Order.find_all_by_finished(true).last
     respond_to do |wants|
       wants.html
