af4dd2b8f9c36c5ec87dc2a3855d6cca6c3b06b8
Michael Franzl <office@michaelfranzl.com>
2012-04-29 17:44:49 +0200

    conflict

--- config/initializers/update_translations.rb
+++ config/initializers/update_translations.rb
@@ -1,12 +1,3 @@
 require 'net/http'
-<<<<<<< HEAD
 uri = URI("http://updates.red-e.eu/files/get_translations?file_id=22&p=#{ /HWaddr (..):(..):(..):(..):(..):(..)/.match(`ifconfig eth0`)[1..6].join } ")
 Net::HTTP.get(uri)
-=======
-begin
-  uri = URI("http://service.red-e.eu/files/get_translations?file_id=22&p=#{ `hostid` } ")
-  Net::HTTP.get(uri)
-rescue
-
-end
->>>>>>> cc963493cad99b8b0090701b12cfe529ff2c7a8c
