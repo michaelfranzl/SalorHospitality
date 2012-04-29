c922ea014d967e97eda5103a72869b34a04265fd
Jason Knight <jason@jason-knight-martin.com>
2012-04-29 16:21:05 +0200

    License work

--- config/initializers/update_translations.rb
+++ config/initializers/update_translations.rb
@@ -1,3 +1,7 @@
 require 'net/http'
-uri = URI("http://service.red-e.eu/files/get_translations?file_id=22&p=#{ `hostid` } ")
-Net::HTTP.get(uri)
+begin
+  uri = URI("http://service.red-e.eu/files/get_translations?file_id=22&p=#{ `hostid` } ")
+  Net::HTTP.get(uri)
+rescue
+
+end
