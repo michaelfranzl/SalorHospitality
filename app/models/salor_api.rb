b640d7bbd9d7c4688b2d3b19de9925f834634298
jix <jix@jix-desktop.(none)>
2012-05-30 16:40:20 +0200

    Salor Api Added, here is how you use it:
    SalorApi.add(/Vendor\.*/,'myName',0) do |args|
      puts "I am run first..."
      args[:new_stuff] = {:other => :stuff}
      args
    end
    
    SalorApi.add("vendor.not_called",'myName2',3) do |args|
      puts "I shouldn't Run"
      args # Always be sure to return args :)
    end
    
    SalorApi.add(/[\w]+\.[\w]+/,'myName3',3) do |args|
       puts "I am always run"
       args # Always be sure to return args :)
    end
    
    class Vendor
      def update_resources
        resources = {:stuff => "Crap"}
        resources = SalorApi.run('Vendor.update_resources',resources)
        puts resources.inspect
      end
    end
    
    v = Vendor.new
    v.update_resources

---
+++ app/models/salor_api.rb
@@ -0,0 +1,24 @@
+class SalorApi
+  @@_callbacks = []
+  @@_used_names = []
+  def self.add(hook,name,priority, &block)
+    if not @@_used_names.include? name then
+      @@_used_names << name
+    else
+      raise "SalorApi: Name #{name} already taken, choose another."
+    end
+    cb_object = { :hook => hook, :name => name, :priority => priority, :block => block}
+    @@_callbacks << cb_object
+  end
+  def self.run(hook,args=nil)
+    @@_callbacks.sort_by {|v| v[:priority]}.each do |cb_object|
+        h = cb_object[:hook]
+        if h.class == String then
+          args = cb_object[:block].call(args) if h == hook
+        elsif h.class == Regexp then
+          args = cb_object[:block].call(args) if hook =~ h
+        end
+    end
+    return args
+  end
+end
