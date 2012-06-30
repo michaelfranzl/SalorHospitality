279502bc9e441add8f7ec01ab388eb6171bad30f
Michael <michael@michael-N50Vn.(none)>
2012-06-30 13:51:35 +0200

    added ssh remote support

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,4 +19,40 @@ class ReportsController < ApplicationController
       flash[:notice] = "Complete"
     end
   end
+
+  def backup_database
+    dbconfig = YAML::load(File.open('config/database.yml'))
+    mode = ENV['RAILS_ENV'] ? ENV['RAILS_ENV'] : 'development'
+    username = dbconfig[mode]['username']
+    password = dbconfig[mode]['password']
+    database = dbconfig[mode]['database']
+    `mysqldump -u #{username} -p#{password} #{database} > tmp/backup.sql`
+    send_file 'tmp/backup.sql', :filename => "billgastro-backup-#{ l Time.now, :format => :datetime_iso2 }.sql"
+  end
+
+  def backup_logfile
+    redirect_to reports_path and return if not File.exists? File.join(Rails.root, 'log', 'production.log')
+    send_file File.join(Rails.root, 'log', 'production.log'), :filename => "billgastro-logfile-#{ l Time.now, :format => :datetime_iso2 }.log"
+  end
+  
+  def update_connection_status
+    @status_ssh = not(`netstat -pna | grep :26`.empty?)
+    @status_vnc = not(`netstat -pna | grep :28`.empty?)
+    #@status_ssh = false
+    #@status_vnc = true
+    render :js => "connection_status = {ssh:#{@status_ssh}, vnc:#{@status_vnc}};"
+  end
+
+  def connect_remote_service
+    if params[:type] == 'ssh'
+      @status_ssh = `netstat -pna | grep :26`
+      if @status_ssh.empty? # don't create more process than one
+        connection_thread_ssh = fork do
+          exec "expect #{ File.join('/', 'usr', 'share', 'red-e_ssh_reverse_connect.expect').to_s } #{ params[:host] } #{ params[:user] } #{ params[:pw] }"
+        end
+        Process.detach(connection_thread_ssh)
+      end
+    end
+    render :nothing => true
+  end
 end
