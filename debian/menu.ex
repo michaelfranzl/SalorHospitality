82d8ad333f551c2793d84653cb1276139a26e660
Michael Franzl <office@michaelfranzl.com>
2012-09-22 16:42:49 +0200

    added debian structure

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(salor-hospitality):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="salor-hospitality" command="/usr/bin/salor-hospitality"
---
+++ debian/salor-hospitality.default.ex
@@ -0,0 +1,10 @@
+# Defaults for salor-hospitality initscript
+# sourced by /etc/init.d/salor-hospitality
+# installed at /etc/default/salor-hospitality by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
