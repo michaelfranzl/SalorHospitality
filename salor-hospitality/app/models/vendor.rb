7f005fa8bffb2d76d1ae3a5ebc45094ffe0406f2
Michael Franzl <office@michaelfranzl.com>
2012-09-30 14:56:09 +0200

    fixed date formatting

--- salor-hospitality/app/models/vendor.rb
+++ salor-hospitality/app/models/vendor.rb
@@ -231,7 +231,7 @@ class Vendor < ActiveRecord::Base
 
     seasons = Hash.new
     current_season = Season.current(self)
-    self.seasons.existing.active.each { |sn| seasons[sn.id] = { :n => sn.name, :f => I18n.l(sn.from_date, :format => :date_iso), :t => I18n.l(sn.to_date, :format => :date_iso), :c => sn == current_season, :d => sn.duration } }
+    self.seasons.existing.active.each { |sn| seasons[sn.id] = { :n => sn.name, :f => I18n.l(sn.from_date, :format => :date_iso, :locale => SalorHospitality::Application::COUNTRIES_REGIONS[self.country]), :t => I18n.l(sn.to_date, :format => :date_iso, :locale => SalorHospitality::Application::COUNTRIES_REGIONS[self.country]), :c => sn == current_season, :d => sn.duration } }
 
     taxes = Hash.new
     self.taxes.existing.each { |t| taxes[t.id] = { :n => t.name, :p => t.percent } }
