ec83d58b0d21638cc32803da8eb17f0718052cee
Michael Franzl <office@michaelfranzl.com>
2012-10-01 15:20:58 +0200

    conditional migration

--- salor-hospitality/db/migrate/20111012125408_add_tables_to_users.rb
+++ salor-hospitality/db/migrate/20111012125408_add_tables_to_users.rb
@@ -1,6 +1,7 @@
 class AddTablesToUsers < ActiveRecord::Migration
   def self.up
     User.all.each do |u|
+      next if u.tables.any?
       u.tables = []
       Table.all.each do |t|
         puts "Adding table #{ t.id } to user #{ u.id }."
