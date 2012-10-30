26f9c0de8e8fefeadb01fde61f81739c33ff05e7
Michael Franzl <office@michaelfranzl.com>
2012-10-30 17:00:10 +0100

    migration fix

--- salor-hospitality/db/migrate/20121007154459_copy_settlement_id_to_items.rb
+++ salor-hospitality/db/migrate/20121007154459_copy_settlement_id_to_items.rb
@@ -2,8 +2,8 @@ class CopySettlementIdToItems < ActiveRecord::Migration
   def up
     Order.all.each do |o|
       puts "Updating Items and TaxItems with settlement_id of order #{ o.id }"
-      Item.where(:order_id => o.id, :settlement_id => nil).update_all :settlement_id => o.settlement_id
-      TaxItem.where(:order_id => o.id, :settlement_id => nil).update_all :settlement_id => o.settlement_id
+      Item.where(:order_id => o.id).update_all :settlement_id => o.settlement_id
+      TaxItem.where(:order_id => o.id).update_all :settlement_id => o.settlement_id
     end
   end
 
