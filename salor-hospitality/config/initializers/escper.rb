2808efc0febffd381c836596751712bc18cbfa2e
Michael Franzl <office@michaelfranzl.com>
2012-11-24 00:51:49 +0100

    new escper version

--- salor-hospitality/config/initializers/escper.rb
+++ salor-hospitality/config/initializers/escper.rb
@@ -1,3 +1,9 @@
 Escper.setup do |config|
   config.codepage_file = File.join(Rails.root, 'config', 'codepages.yml')
+  
+  if @mode != 'local' and SalorHospitality::Application::SH_DEBIAN_SITEID != 'none'
+    config.use_safe_device_path = true
+    config.safe_device_path = File.join('/', 'var', 'lib', 'salor-hospitality', SalorHospitality::Application::SH_DEBIAN_SITEID, 'public', 'uploads')
+    #config.safe_device_path = File.join('/', 'tmp')
+  end
 end
