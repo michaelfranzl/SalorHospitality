e4cdd0125698cfcc65bfca7f120fa331bcdd25ea
Michael Franzl <office@michaelfranzl.com>
2012-11-24 01:28:16 +0100

    fix

--- salor-hospitality/config/initializers/escper.rb
+++ salor-hospitality/config/initializers/escper.rb
@@ -1,9 +1,9 @@
 Escper.setup do |config|
   config.codepage_file = File.join(Rails.root, 'config', 'codepages.yml')
   
-  if @mode != 'local' and SalorHospitality::Application::SH_DEBIAN_SITEID != 'none'
-    config.use_safe_device_path = true
+  config.use_safe_device_path = SalorHospitality::Application::CONFIGURATION[:use_safe_device_path] == true ? true : false
+  
+  if SalorHospitality::Application::SH_DEBIAN_SITEID != 'none'
     config.safe_device_path = File.join('/', 'var', 'lib', 'salor-hospitality', SalorHospitality::Application::SH_DEBIAN_SITEID, 'public', 'uploads')
-    #config.safe_device_path = File.join('/', 'tmp')
   end
 end
