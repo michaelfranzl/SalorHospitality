6098997c0d391afc0376b292212c1c5307b756ce
Michael Franzl <office@michaelfranzl.com>
2012-12-01 14:16:27 +0100

    utf8

--- debian/salor-hospitality.cron.d
+++ debian/salor-hospitality.cron.d
@@ -1,4 +1,5 @@
 #
 # Regular cron jobs for the salor-hospitality package
 #
-0 4 * * * root /usr/bin/salor-maintainance h
+0 2 * * * root test -x /usr/bin/salor-maintainance && /usr/bin/salor-maintainance h
+0 3 * * * root test -x /usr/bin/salor-remote-backup && /usr/bin/salor-remote-backup h
