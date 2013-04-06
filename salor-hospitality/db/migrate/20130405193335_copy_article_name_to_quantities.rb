6b297ab8d7af7d91774ed499e12fff84d80fca3e
Michael Franzl <office@michaelfranzl.com>
2013-04-06 08:26:14 +0200

    fix in migration

--- salor-hospitality/db/migrate/20130405193335_copy_article_name_to_quantities.rb
+++ salor-hospitality/db/migrate/20130405193335_copy_article_name_to_quantities.rb
@@ -1,6 +1,7 @@
 class CopyArticleNameToQuantities < ActiveRecord::Migration
   def up
     Quantity.all.each do |q|
+      next unless q.article
       puts "Copying artile name #{ q.article.name } to quantity #{ q.id }"
       q.article_name = q.article.name
       q.save
