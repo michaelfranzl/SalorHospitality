86130c89e60913042cf89a88e188bbd160141586
Michael Franzl <office@michaelfranzl.com>
2013-06-13 16:12:09 +0200

    added printing url/password help to vendors screen

--- debian/salor-hospitality.cron.d
+++ debian/salor-hospitality.cron.d
@@ -1,5 +1,5 @@
 #
 # Regular cron jobs for the salor-hospitality package
 #
-0 2 * * * root test -x /usr/bin/salor-maintainance && /usr/bin/salor-maintainance h
+0 2 * * * root test -x /usr/bin/salor-maintenance && /usr/bin/salor-maintenance h
 0 3 * * * root test -x /usr/bin/salor-remote-backup && /usr/bin/salor-remote-backup h
