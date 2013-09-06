d465b648b5491f4e5f5391ed5349c8ca719ceeca
BretonFin <brett_sw_eng@yahoo.ca>
2013-09-06 14:44:32 +0200

    the application.rb possibly needs merging

--- salor-hospitality/config/application.rb
+++ salor-hospitality/config/application.rb
@@ -11,6 +11,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'socket'
+require 'pp'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -19,15 +21,36 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+
+
+  
 module SalorHospitality
-  class Application < Rails::Application
-    
+  
+  mattr_accessor :tailor
+  mattr_accessor :old_tailors
+  mattr_accessor :requestcount
+  
+  @@tailor = nil
+  @@old_tailors = []
+  @@requestcount = 0
+  
+  
+  class Application < Rails::Application  
     if ENV['SH_DEBIAN_SITEID']
       SH_DEBIAN_SITEID = ENV['SH_DEBIAN_SITEID']
     else
       SH_DEBIAN_SITEID = 'none'
     end
-
+    if File.exists? '/etc/redhat-release' then
+      SH_DEPLOY = 'REDHAT'
+      OS_RELEASE = `cat /etc/redhat-release`
+    elsif File.exists? '/etc/debian_version' then
+      SH_DEPLOY = 'DEBIAN'
+      OS_RELEASE = `cat /etc/debian_version`
+    elsif File.exists? '/mach_kernel' then
+      SH_DEPLOY = 'MAC'
+      OS_RELEASE = `uname -r`
+    end
     puts "Using database set by environment variable SH_DEBIAN_SITEID (#{SH_DEBIAN_SITEID})"
     
     if File.exists?(File.join(Rails.root, '..', 'debian', 'changelog'))
@@ -43,14 +66,43 @@ module SalorHospitality
       CONFIGURATION = YAML::load(File.open(File.join(Rails.root, 'config', 'config.yml'), 'r').read)
     end
     
-    raise "The setting :branding_codename in config.yml only supports the values 'billgastro' or 'salorhospitality'" unless ['salorhospitality','billgastro'].include?(CONFIGURATION[:branding_codename])
+    #raise "The setting :branding_codename in config.yml only supports the values 'billgastro' or 'salorhospitality'" unless ['salorhospitality','billgastro'].include?(CONFIGURATION[:branding_codename])
 
-    LANGUAGES = { 'en' => 'English', 'gn' => 'Deutsch', 'tr' => 'Türkçe', 'fr' => 'Français', 'es' => 'Español', 'pl' => 'Polski', 'hu' => 'Magyar', 'el' => 'Greek', 'ru' => 'Русский', 'it' => 'Italiana', 'cn' => 'Chinese', 'hk' => 'Hongkong', 'tw' => 'Taiwanese'}
-    COUNTRIES = { 'cc' => :default, 'de' => 'Deutschland', 'at' => 'Österreich', 'tr' => 'Türkiye', 'fr' => 'France', 'es' => 'España', 'pl' => 'Polska', 'hu' => 'Magyarország', 'el' => 'Ελλάδα', 'ru' => 'Россия', :it => 'Italia', 'cn' => 'China', 'hk' => 'Hongkong', 'tw' => 'Taiwan', 'us' => 'USA', 'gb' => 'England' }
-    COUNTRIES_REGIONS = { 'cc' => 'en-us', 'de' => 'gn-de', 'at' => 'gn-de', 'tr' => 'tr-tr', 'fr' => 'fr-fr', 'es' => 'es-es', 'pl' => 'pl-pl', 'hu' => 'hu-hu', 'el' => 'el-el', 'ru' => 'ru-ru', 'it' => 'it-it', 'cn' => 'cn-cn', 'us' => 'en-us', 'gb' => 'en-gb', 'hk' => 'hk-hk', 'tw' => 'tw-tw'}
-    COUNTRIES_INVOICES = { 'cc' => 'cc', 'de' => 'cc', 'at' => 'cc', 'tr' => 'cc', 'fr' => 'cc', 'es' => 'cc', 'pl' => 'pl', 'hu' => 'cc', 'el' => 'cc', 'ru' => 'cc', 'it' => 'cc', 'cn' => 'cc', 'us' => 'cc', 'gb' => 'cc', 'hk' => 'cc', 'tw' => 'cc' }
+<<<<<<< HEAD
+    LANGUAGES = { 'en' => 'English', 'gn' => 'Deutsch', 'tr' => 'Türkçe', 'fr' => 'Français', 'es' => 'Español', 'pl' => 'Polski', 'hu' => 'Magyar', 'el' => 'Greek', 'ru' => 'Русский', 'it' => 'Italiana', 'cn' => 'Chinese', 'hk' => 'Hongkong', 'tw' => 'Taiwanese','fi' => 'Suomeksi'}
+    COUNTRIES = { 'cc' => :default, 'de' => 'Deutschland', 'at' => 'Österreich', 'tr' => 'Türkiye', 'fr' => 'France', 'es' => 'España', 'pl' => 'Polska', 'hu' => 'Magyarország', 'el' => 'Ελλάδα', 'ru' => 'Россия', :it => 'Italia', 'cn' => 'China', 'hk' => 'Hongkong', 'tw' => 'Taiwan', 'us' => 'USA', 'gb' => 'England','fi' => 'Suomi' }
+    COUNTRIES_REGIONS = { 'cc' => 'en-us', 'de' => 'gn-de', 'at' => 'gn-de', 'tr' => 'tr-tr', 'fr' => 'fr-fr', 'es' => 'es-es', 'pl' => 'pl-pl', 'hu' => 'hu-hu', 'el' => 'el-el', 'ru' => 'ru-ru', 'it' => 'it-it', 'cn' => 'cn-cn', 'us' => 'en-us', 'gb' => 'en-gb', 'hk' => 'hk-hk', 'tw' => 'tw-tw', 'fi' => 'fi-fi'}
+    COUNTRIES_INVOICES = { 'cc' => 'cc', 'de' => 'cc', 'at' => 'cc', 'tr' => 'cc', 'fr' => 'cc', 'es' => 'cc', 'pl' => 'pl', 'hu' => 'cc', 'el' => 'cc', 'ru' => 'cc', 'it' => 'cc', 'cn' => 'cc', 'us' => 'cc', 'gb' => 'cc', 'hk' => 'cc', 'tw' => 'cc', 'fi' => 'cc' }
+=======
+    LANGUAGES = { 'en' => 'English', 'gn' => 'Deutsch', 'tr' => 'Türkçe', 'fr' => 'Français', 'es' => 'Español', 'pl' => 'Polski', 'hu' => 'Magyar', 'el' => 'Greek', 'ru' => 'Русский', 'it' => 'Italiana', 'cn' => 'Chinese', 'hk' => 'Hongkong', 'tw' => 'Taiwanese', 'vi' => 'tiếng Việt'}
+    COUNTRIES = { 'cc' => :default, 'de' => 'Deutschland', 'at' => 'Österreich', 'ch' => 'Schweiz', 'tr' => 'Türkiye', 'fr' => 'France', 'es' => 'España', 'pl' => 'Polska', 'hu' => 'Magyarország', 'el' => 'Ελλάδα', 'ru' => 'Россия', :it => 'Italia', 'cn' => '中国', 'hk' => 'Hongkong', 'tw' => 'Taiwan', 'us' => 'USA', 'gb' => 'England', 'vi' => 'Việt Nam', 'kh' => 'ព្រះរាជាណាចក្រកម្ពុជា'}
+    COUNTRIES_REGIONS = { 'cc' => 'en-us', 'de' => 'gn-de', 'at' => 'gn-de', 'ch' => 'gn-ch', 'tr' => 'tr-tr', 'fr' => 'fr-fr', 'es' => 'es-es', 'pl' => 'pl-pl', 'hu' => 'hu-hu', 'el' => 'el-el', 'ru' => 'ru-ru', 'it' => 'it-it', 'cn' => 'cn-cn', 'us' => 'en-us', 'gb' => 'en-gb', 'hk' => 'hk-hk', 'tw' => 'tw-tw', 'vi' => 'vi-vi', 'kh' => 'kh-kh'}
+    COUNTRIES_INVOICES = { 'cc' => 'cc', 'de' => 'cc', 'at' => 'cc', 'ch' => 'cc', 'tr' => 'cc', 'fr' => 'cc', 'es' => 'cc', 'pl' => 'pl', 'hu' => 'cc', 'el' => 'cc', 'ru' => 'cc', 'it' => 'cc', 'cn' => 'cc', 'us' => 'cc', 'gb' => 'cc', 'hk' => 'cc', 'tw' => 'cc', 'vi' => 'cc', 'kh' => 'cc' }
+    
+>>>>>>> 4f74b497e659bf3116a1954b56282a0878920615
     FONTS = Dir.glob(File.join(Rails.root,'public','fonts','*.ttf')).collect{ |f| "#{ /fonts\/(.*).ttf/.match(f)[1]}" }
-    PERMISSIONS = ['take_orders','reactivate_orders','decrement_items','delete_items','cancel_all_items_in_active_order','finish_orders','order_notes','split_items','move_tables','assign_order_to_user','refund','assign_cost_center','assign_order_to_booking','move_order','move_order_from_invoice_form','manage_articles','manage_categories','manage_statistic_categories','manage_options','add_option_to_sent_item','finish_all_settlements','finish_own_settlement','view_all_settlements','settlement_statistics_taxes','settlement_statistics_taxes_categories','manage_business_invoice','manage_users','manage_taxes','manage_cost_centers','manage_payment_methods','manage_tables','manage_vendors','manage_cameras','counter_mode','see_item_notifications_user_preparation','see_item_notifications_user_delivery','see_item_notifications_vendor_preparation','see_item_notifications_vendor_delivery','see_item_notifications_static','manage_pages','manage_customers','manage_hotel','manage_roles','item_scribe','assign_tables','download_database','remote_support','login_locking','mobile_show_tools','receipt_logo','manage_statistics','manage_statistics_hotel','statistics_weekday','statistics_hour','statistics_table','statistics_payment_method','statistics_category','statistics_statistic_category','statistics_tax','allow_exit','see_debug']
+    PERMISSIONS = [
+      'take_orders','reactivate_orders','decrement_items','delete_items',
+      'cancel_all_items_in_active_order','finish_orders','order_notes',
+      'split_items','move_tables','assign_order_to_user','refund',
+      'assign_cost_center','assign_order_to_booking','move_order','interim_receipt',
+      'move_order_from_invoice_form','manage_articles','manage_categories',
+      'manage_statistic_categories','manage_options','add_option_to_sent_item',
+      'finish_all_settlements','finish_own_settlement','view_all_settlements',
+      'view_settlements_table','settlement_statistics_taxes',
+      'settlement_statistics_taxes_categories','settlement_statistics_sold_quantities',
+      'manage_business_invoice','manage_users','manage_taxes','manage_cost_centers',
+      'manage_payment_methods','manage_tables','manage_vendors','manage_cameras',
+      'counter_mode','immediate_order_finish','see_item_notifications_user_preparation','see_item_notifications_user_delivery',
+      'see_item_notifications_vendor_preparation','see_item_notifications_vendor_delivery',
+      'see_item_notifications_static','manage_pages','manage_customers','manage_hotel',
+      'manage_roles','item_scribe','assign_tables','download_database','download_csv',
+      'remote_support','login_locking','mobile_show_tools','receipt_logo','manage_statistics',
+      'manage_statistics_hotel','statistics_weekday','statistics_hour','statistics_table',
+      'statistics_payment_method','statistics_category','statistics_statistic_category',
+      'statistics_tax','statistics_sold_quantities','allow_exit','assign_users_to_vendors',
+      'manage_advertising','manage_user_logins','see_debug'
+    ]
     
     if SalorHospitality::Application::CONFIGURATION[:exception_notification] == true
       require File.join(Rails.root, 'lib', 'exceptions.rb')
@@ -75,7 +127,7 @@ module SalorHospitality
 
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-    #config.active_record.observers = [:history_observer]
+    config.active_record.observers = :history_observer
     
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
