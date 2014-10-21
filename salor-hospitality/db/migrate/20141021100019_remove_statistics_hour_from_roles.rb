c753c905e2679edbc36a0ed959ec2b15503856a4
Michael Franzl <office@michaelfranzl.com>
2014-10-21 14:12:37 +0200

    fixed typo in migration

--- salor-hospitality/db/migrate/20141021100019_remove_statistics_hour_from_roles.rb
+++ salor-hospitality/db/migrate/20141021100019_remove_statistics_hour_from_roles.rb
@@ -2,7 +2,7 @@ class RemoveStatisticsHourFromRoles < ActiveRecord::Migration
   def up
     Role.all.each do |r|
       r.permissions.delete("statistics_hour")
-      resut = r.save
+      result = r.save
       puts "Removing permission statistics_hour from Role ID #{ r.id }. Save result was #{ result }"
     end
   end
