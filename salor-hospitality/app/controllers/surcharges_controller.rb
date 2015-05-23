be735003f18173def3c75181b0edf5ee194ee33c
Michael Franzl <office@michaelfranzl.com>
2015-05-23 20:26:29 +0200

    fix for surcharge display

--- salor-hospitality/app/controllers/surcharges_controller.rb
+++ salor-hospitality/app/controllers/surcharges_controller.rb
@@ -14,6 +14,7 @@ class SurchargesController < ApplicationController
   def index
     season_ids = @current_vendor.seasons.existing.collect { |s| s.id }
     guest_type_ids = @current_vendor.guest_types.collect{ |gt| gt.id }
+    guest_type_ids << nil
     
     @surcharges = @current_vendor.surcharges.existing.where(:season_id => season_ids, :guest_type_id => guest_type_ids)
     @surcharge_names = @surcharges.collect{ |s| s.name }.uniq
