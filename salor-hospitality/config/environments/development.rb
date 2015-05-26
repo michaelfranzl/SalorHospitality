a96e6b6e4cadd854f886c71c85bd23c44dd96015
Michael Franzl <office@michaelfranzl.com>
2015-05-26 16:40:53 +0200

    remove require_reloader gem, bump rmagick gem version

--- salor-hospitality/config/environments/development.rb
+++ salor-hospitality/config/environments/development.rb
@@ -1,8 +1,6 @@
 SalorHospitality::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   
-  RequireReloader.watch_local_gems!
-  
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
